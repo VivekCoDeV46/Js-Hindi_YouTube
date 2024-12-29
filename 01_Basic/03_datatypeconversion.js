311f003160700abf5e0a03b4be3072e1002b4021
VivekCoDeV46 <vivek.contact46@gmail.com>
2024-12-29 08:02:27 +0000

    Operation

--- 01_Basic/03_datatypeconversion.js
+++ 01_Basic/03_datatypeconversion.js
@@ -1,49 +1,49 @@
-let score1 = "46"
-let score2 = "46demon"
-let score3 = null;
-let score4 = undefined;
-let score5 = false
-let score6;
+// let score1 = "46"
+// let score2 = "46demon"
+// let score3 = null;
+// let score4 = undefined;
+// let score5 = false
+// let score6;
 
-console.log(typeof score1);
-console.log(typeof (score1));
+// console.log(typeof score1);
+// console.log(typeof (score1));
 
-console.log(typeof score2);
-console.log(typeof (score2));
+// console.log(typeof score2);
+// console.log(typeof (score2));
 
-console.log(typeof score3);
-console.log(typeof (score3));
+// console.log(typeof score3);
+// console.log(typeof (score3));
 
-console.log(typeof score4);
-console.log(typeof (score4));
+// console.log(typeof score4);
+// console.log(typeof (score4));
 
-console.log(typeof score5);
-console.log(typeof (score5));
+// console.log(typeof score5);
+// console.log(typeof (score5));
 
-console.log(typeof score6);
-console.log(typeof (score6));
+// console.log(typeof score6);
+// console.log(typeof (score6));
 
 
-let inNumberScore1 = Number(score1);
-let inNumberScore2 = Number(score2);
-let inNumberScore3 = Number(score3);
-let inNumberScore4 = Number(score4);
-let inNumberScore5 = Number(score5);
-let inNumberScore6 = Number(score6);
+// let inNumberScore1 = Number(score1);
+// let inNumberScore2 = Number(score2);
+// let inNumberScore3 = Number(score3);
+// let inNumberScore4 = Number(score4);
+// let inNumberScore5 = Number(score5);
+// let inNumberScore6 = Number(score6);
 
-console.log(typeof inNumberScore1);
-console.log(typeof inNumberScore2);
-console.log(typeof inNumberScore3);
-console.log(typeof inNumberScore4);
-console.log(typeof inNumberScore5);
-console.log(typeof inNumberScore6);
+// console.log(typeof inNumberScore1);
+// console.log(typeof inNumberScore2);
+// console.log(typeof inNumberScore3);
+// console.log(typeof inNumberScore4);
+// console.log(typeof inNumberScore5);
+// console.log(typeof inNumberScore6);
 
-console.log(inNumberScore1);
-console.log(inNumberScore2);
-console.log(inNumberScore3);
-console.log(inNumberScore4);
-console.log(inNumberScore5);
-console.log(inNumberScore6);
+// console.log(inNumberScore1);
+// console.log(inNumberScore2);
+// console.log(inNumberScore3);
+// console.log(inNumberScore4);
+// console.log(inNumberScore5);
+// console.log(inNumberScore6);
 
 //"33" = 33
 //"33" = NaN
@@ -52,21 +52,59 @@ console.log(inNumberScore6);
 //Undefined = NaN
 //only variable = NaN
 
-let inLoggedIn = 1;
-let inLoggedIn2 = "";
-let inLoggedIn3 = "Vivek";
-let booleanInLoggedIn = Boolean(inLoggedIn)
-let booleanInLoggedIn2 = Boolean(inLoggedIn2)
-let booleanInLoggedIn3 = Boolean(inLoggedIn3)
- console.table([typeof booleanInLoggedIn, typeof booleanInLoggedIn2, typeof booleanInLoggedIn3]);
- console.table([booleanInLoggedIn, booleanInLoggedIn2, booleanInLoggedIn3]);
+// let inLoggedIn = 1;
+// let inLoggedIn2 = "";
+// let inLoggedIn3 = "Vivek";
+// let booleanInLoggedIn = Boolean(inLoggedIn)
+// let booleanInLoggedIn2 = Boolean(inLoggedIn2)
+// let booleanInLoggedIn3 = Boolean(inLoggedIn3)
+//  console.table([typeof booleanInLoggedIn, typeof booleanInLoggedIn2, typeof booleanInLoggedIn3]);
+//  console.table([booleanInLoggedIn, booleanInLoggedIn2, booleanInLoggedIn3]);
 
  //1 = true; 0 = false
  //"" = false
  // "Vivek" = true
 
- let someNumber = 33;
- let StringSomeNumber = String(someNumber);
+//  let someNumber = 33;
+//  let StringSomeNumber = String(someNumber);
 
- console.log(StringSomeNumber);
- console.log(typeof StringSomeNumber);
+//  console.log(StringSomeNumber);
+//  console.log(typeof StringSomeNumber);
+
+ //*********************************** Operation *************************************//
+
+let number = 46;
+let negNumber = -number;
+// console.log(negNumber);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let str1 = "Namaste "
+let str2 = "Vivek Prajapati"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(4 + 5 * 6 % 3);
+
+// console.log(true); //ture
+// console.log(+true); //1
+// console.log(+""); //0
+
+
+let counterGame = 45;
+let finishGame = 45;
+counterGame++;
+++finishGame;
+console.log("counterGame = ", counterGame);
+console.log("finishGame = ", finishGame);
