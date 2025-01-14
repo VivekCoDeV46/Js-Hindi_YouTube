47f388d52261f2c50c7d3f66f791c7af5e5641da
VivekCoDeV46 <vivek.contact46@gmail.com>
2025-01-14 17:49:40 +0000

    Num and Math

--- 01_Basic/07_Nums_and_Maths.js
+++ 01_Basic/07_Nums_and_Maths.js
@@ -1,40 +1,49 @@
-const player = 46;
+// const player = 46;
 
-console.log(player);
+// console.log(player);
 
-const balance = new Number(46);
-console.log(balance);
+// const balance = new Number(46);
+// console.log(balance);
 
-console.log(balance.toString().length)
-console.log(balance.toFixed(2));
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2));
 
-const otherPlayer = 46.46;
-console.log(otherPlayer.toPrecision(2));
-const otherPlayer2 = 46.66;
-console.log(otherPlayer2.toPrecision(2));
-const otherPlayer3 = 146.96;
-console.log(otherPlayer3.toPrecision(4))
+// const otherPlayer = 46.46;
+// console.log(otherPlayer.toPrecision(2));
+// const otherPlayer2 = 46.66;
+// console.log(otherPlayer2.toPrecision(2));
+// const otherPlayer3 = 146.96;
+// console.log(otherPlayer3.toPrecision(4))
 
-const price = 100000;
-console.log(price.toLocaleString());
-console.log(price.toLocaleString('en-IN'));
+// const price = 100000;
+// console.log(price.toLocaleString());
+// console.log(price.toLocaleString('en-IN'));
 
 
-//```````````````````````````````````` Maths ````````````````````````````````````
-console.log(Math);
-console.log(Math.abs(-46));
-console.log(Math.abs(46));
-console.log(Math.round(46.2));
-console.log(Math.round(46.8));
-console.log(Math.ceil(46.2));
-console.log(Math.ceil(46.8));
-console.log(Math.floor(46.2));
-console.log(Math.floor(46.8));
+// //```````````````````````````````````` Maths ````````````````````````````````````
+// console.log(Math);
+// console.log(Math.abs(-46));
+// console.log(Math.abs(46));
+// console.log(Math.round(46.2));
+// console.log(Math.round(46.8));
+// console.log(Math.ceil(46.2));
+// console.log(Math.ceil(46.8));
+// console.log(Math.floor(46.2));
+// console.log(Math.floor(46.8));
 
-console.log(Math.min(5,7,3,6,3,2,1.5));
-console.log(Math.max(5,7,3,6,3,2,1.5));
+// console.log(Math.min(5,7,3,6,3,2,1.5));
+// console.log(Math.max(5,7,3,6,3,2,1.5));
 
 console.log(Math.random());
-console.log(Math.random());
+console.log(Math.random()*8);
+console.log(Math.random()*8 + 1);
+console.log((Math.random()*8)+ 1);
+console.log(Math.floor(Math.random()*8)+ 1);
 
-Math.random
+const min = 10;
+const max = 20;
+
+console.log(Math.random()*(min + max))
+console.log(Math.random()*(min + max)+min)
+console.log(Math.floor(Math.random()*(min + max)+min))
+console.log(Math.ceil(Math.random()*(min + max)+min))
