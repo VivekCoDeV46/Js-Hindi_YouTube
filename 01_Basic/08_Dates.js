bc97d0fe81f70709859323bb4c87bd7db7b98f8a
VivekCoDeV46 <vivek.contact46@gmail.com>
2025-01-18 18:18:49 +0000

    Dates

---
+++ 01_Basic/08_Dates.js
@@ -0,0 +1,20 @@
+// Dates
+
+// let myDate = new Date()
+// console.log("toString :", myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleTimeString());
+// console.log(typeof myDate);
+
+// let myCreateDate = new Date(2024, 0, 23);
+
+// let myCreateDate = new Date(2024, 0, 23, 4 , 6);
+
+// let myCreateDate = new Date(2024, 0, 23);
+
+let myCreateDate = new Date("01-14-2024");
+console.log(myCreateDate.toLocaleString());
+
