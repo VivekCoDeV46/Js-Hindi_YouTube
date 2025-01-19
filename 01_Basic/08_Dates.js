29b69bf20b6dbcec44f5a87efad3d41da3ca9649
VivekCoDeV46 <vivek.contact46@gmail.com>
2025-01-19 18:44:25 +0000

    Dates

--- 01_Basic/08_Dates.js
+++ 01_Basic/08_Dates.js
@@ -16,5 +16,26 @@
 // let myCreateDate = new Date(2024, 0, 23);
 
 let myCreateDate = new Date("01-14-2024");
-console.log(myCreateDate.toLocaleString());
+// console.log(myCreateDate.toLocaleString());
 
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+let newDate = new Date();
+// console.log(newDate);
+// console.log(newDate.getDate());
+// console.log(newDate.getDay() + 1);
+// console.log(newDate.getFullYear());
+// console.log(newDate.getMilliseconds());
+// console.log(newDate.getSeconds());
+// console.log(newDate.getMinutes());
+// console.log(newDate.getHours());
+// console.log(newDate.getMonth() + 1);
+
+// console.log(`Time is ${newDate.getTime()}`);
+
+console.log(newDate.toLocaleString('defualt', {
+    weekday: "long"
+}));
